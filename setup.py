20d2d528d387102b6d7513023147891167d7cfda
Vincent Rabaud <vincent.rabaud@gmail.com>
2014-08-19 23:15:57 +0200

    Imported upstream version '0.1.0' of 'upstream'

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from catkin_pkg.python_setup import generate_distutils_setup
+
+d = generate_distutils_setup(
+    packages=['nao_sensors'],
+    package_dir={'': 'src'},
+    scripts=['nodes/camera.py', 'nodes/octomap.py', 'nodes/sonar.py']
+)
+
+setup(**d)
